25ceb7d0e940ce5e91d0c3cad6a6e05b17f505a5
Nurlan Seitov <seitov@gmail.com>
2023-03-05 01:39:02 +0600

    Initial Commit

---
+++ Test_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_task
+//
+//  Created by Nurlan Seitov on 5/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
